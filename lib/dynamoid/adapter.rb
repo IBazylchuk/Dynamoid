9b2f96f1f38b3fa807928f5bf08ee01efa7f15a0
Ilya Bazylchuk <ilya.bazylchuk@gmail.com>
2016-05-02 23:28:43 +0300

    Reset cache tables after create table

--- lib/dynamoid/adapter.rb
+++ lib/dynamoid/adapter.rb
@@ -173,7 +173,8 @@ module Dynamoid
     def create_table(table_name, key, options = {})
       if !tables.include?(table_name)
         benchmark('Create Table') { adapter.create_table(table_name, key, options) }
-        tables << table_name
+        # tables << table_name
+        reset_cache_tables
       end
     end
 
